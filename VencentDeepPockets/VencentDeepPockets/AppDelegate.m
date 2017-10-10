113681d91bc070199f54987f7c81dee19ec123a1
ALPS.K <hankai@MacBook-hankai.local>
2017-10-10 17:04:09 +0800

    LaunchImage 启动图动画

--- VencentDeepPockets/VencentDeepPockets/AppDelegate.m
+++ VencentDeepPockets/VencentDeepPockets/AppDelegate.m
@@ -206,6 +206,31 @@ static BOOL isProduction = FALSE;
     } completion:^(BOOL finished) {
         [launchView removeFromSuperview];
     }];
+    //*****************************LaunchImage*****************************
+    /*
+    CGSize viewSize = self.window.bounds.size;
+    NSString *viewOrientation = @"Portrait";    //横屏请设置成 @"Landscape"
+    NSString *launchImage = nil;
+    
+    NSArray* imagesDict = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"UILaunchImages"];
+    for (NSDictionary* dict in imagesDict) {
+        CGSize imageSize = CGSizeFromString(dict[@"UILaunchImageSize"]);
+        if (CGSizeEqualToSize(imageSize, viewSize) && [viewOrientation isEqualToString:dict[@"UILaunchImageOrientation"]]) {
+            launchImage = dict[@"UILaunchImageName"];
+        }
+    }
+    UIImageView *launchImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:launchImage]];
+    launchImageView.frame = self.window.bounds;
+    launchImageView.contentMode = UIViewContentModeScaleAspectFill;
+    [self.window addSubview:launchImageView];
+    
+    [UIView animateWithDuration:2.0f delay:0.0f options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+         launchImageView.alpha = 0.0f;
+         launchImageView.layer.transform = CATransform3DScale(CATransform3DIdentity, 1.2, 1.2, 1);
+    } completion:^(BOOL finished) {
+         [launchImageView removeFromSuperview];
+    }];
+     */
 }
 
 
